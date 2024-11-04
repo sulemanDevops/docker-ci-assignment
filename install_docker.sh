446b5d38de30aa1e2e8a7b1b67dd67bb2edc6c22
sulemanDevops <mesuleman.dev@gmail.com>
2024-11-04 22:45:38 +0500

    Added docker-compose with bash script

---
+++ install_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Update Homebrew and install Docker
+echo "Updating Homebrew..."
+brew update
+echo "Installing Docker..."
+brew install --cask docker
+echo "Docker installation completed!"
+
